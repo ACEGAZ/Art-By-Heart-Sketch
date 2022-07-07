b2ee3a3e30542917283559f51725573912b40a23
ACEGAZ <garylowles@gmail.com>
2022-07-07 13:46:38 +0000

    install libraries and add requirements.txt file

--- artbyheartsketch/settings.py
+++ artbyheartsketch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-idm-sjeb_=ny0k(y1f@%&f1)-m5&1#ik)6us3ah-0r2xp876-g'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
