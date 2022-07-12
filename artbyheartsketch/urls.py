e53abc717fafb12c3545ebe9aaa1dff74e0731be
ACEGAZ <garylowles@gmail.com>
2022-07-12 09:25:47 +0000

    create super user

--- artbyheartsketch/urls.py
+++ artbyheartsketch/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'website'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='base.html'))
 ]
